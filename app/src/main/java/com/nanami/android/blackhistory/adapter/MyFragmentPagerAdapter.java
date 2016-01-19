e24290108c635f11d56ba423e1baaeebc6fb7067
Atsumi <atsumi3041@yahoo.co.jp>
2016-01-19 15:10:55 +0900

    add MentionsList

--- app/src/main/java/com/nanami/android/blackhistory/adapter/MyFragmentPagerAdapter.java
+++ app/src/main/java/com/nanami/android/blackhistory/adapter/MyFragmentPagerAdapter.java
@@ -9,6 +9,7 @@ import android.support.v4.util.Pair;
 import com.nanami.android.blackhistory.fragment.CommonStreamFragment;
 import com.nanami.android.blackhistory.fragment.FavoriteStreamFragment;
 import com.nanami.android.blackhistory.fragment.HomeStreamFragment;
+import com.nanami.android.blackhistory.fragment.MentionsStreamFragment;
 import com.nanami.android.blackhistory.fragment.list.TimelineListType;
 import com.nanami.android.blackhistory.utils.BHLogger;
 
@@ -68,6 +69,7 @@ public class MyFragmentPagerAdapter extends FragmentStatePagerAdapter {
              * - とりあえず最初にロードした分表示
              */
             case Mentions:
+                additionalFragment = MentionsStreamFragment.newInstance(userId);
                 break;
 
             /**
